4ce13f19368da94c2e08f8b8e44521cd2705d236
sidane17 <khalilsidane89@gmail.com>
2024-04-29 18:12:52 +0200

    pariDispari

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 let p="";
 let c=0;
+let pari;
+let dispari;
 document.getElementById("button").onclick=function(){
     p=document.getElementById("parola").value;
     let pa=p.split("");
@@ -17,4 +19,49 @@ document.getElementById("button").onclick=function(){
     }
      
 console.log(p)
+}
+document.getElementById("pari").onclick=function(){
+    pari=true;
+   
+    
+    
+}
+document.getElementById("dispari").onclick=function(){
+    dispari=true;
+    
+}
+document.getElementById("b_p_d").onclick=function(){
+    let num = document.getElementById("num").value;
+    if (num>5 || num<1 ){
+    document.getElementById("RispostaPariDispari").innerHTML="Numero oltre i limiti o non hai scelto. RIPROVA"
+    }
+    let g= Math.floor(Math.random() * (6 -1) + 1);
+    console.log(g)
+    if((g+num)%0){
+        if(pari==true){
+            document.getElementById("RispostaPariDispari").innerHTML="Ha vinto PARI"
+            console.log(1)
+        }else{
+            document.getElementById("RispostaPariDispari").innerHTML="ha vinto Dispari"
+            console.log(2)
+        }
+        
+
+
+    }else {
+        if(dispari==true){
+            document.getElementById("RispostaPariDispari").innerHTML="Ha vinto DISPARI"
+            console.log(3)
+        }else{
+            document.getElementById("RispostaPariDispari").innerHTML="Ha vinto pari"
+            console.log(4)
+        }
+        
+
+        
+    }
+
+    // if( pari!=true || dispari!=true){
+
+    // }
 }
